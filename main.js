94c89a769f8a540364610c877e5ad954973bde9e
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-12-18 00:44:09 +0700

    Add Status.

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
 let natherine = () => {
     let audioNatherine = document.getElementById("natherinemp3")
-    return audioNatherine.paused ? audioNatherine.play() : audioNatherine.pause()
+    let natherineplay = document.getElementById("natherinePlay")
+    if (audioNatherine.paused) {
+        audioNatherine.play()
+        natherineplay.innerHTML = "isPlaying..."
+    } else {
+        audioNatherine.pause()
+        natherineplay.innerHTML = ""
+    }
 }
 
 let wee = () => {
     let audioWee = document.getElementById("weemp3")
-    return audioWee.paused ? audioWee.play() : audioWee.pause()
+    let weeplay = document.getElementById("weePlay")
+    if(audioWee.paused) {
+        audioWee.play()
+        weeplay.innerHTML = "isPlaying..."
+    }
+    else {
+        audioWee.pause()
+        weeplay.innerHTML = ""
+    }
+    // return audioWee.paused ? audioWee.play() : audioWee.pause()
 }
 
 let tarwaan = () => {
     let audioTarwaan = document.getElementById("tarwaanmp3")
-    return audioTarwaan.paused ? audioTarwaan.play() : audioTarwaan.pause()
+    let tarwaanplay = document.getElementById("tarwaanPlay")
+    if(audioTarwaan.paused) {
+        audioTarwaan.play()
+        tarwaanplay.innerHTML = "isPlaying..."
+    }
+    else {
+        audioTarwaan.pause()
+        tarwaanplay.innerHTML = ""
+    }
 }
