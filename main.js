6171070c00ae7543a8356ccf4e68fade68b7a2ad
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-12-18 00:13:01 +0700

    Add Sound and Remove Alert.

--- main.js
+++ main.js
@@ -1,32 +1,14 @@
 let natherine = () => {
-    // let tagname = document.getElementsByTagName("p")
-    // let audioNatherine = document.createElement("audio")
-    // audioNatherine.setAttribute("src","natherine.mp3")
-    // audioNatherine.setAttribute("preload","auto")
-    alert("Natherine BNK48 เป็นสาวเก่งที่มีชื่อเสียงจากการที่เธอทายผลฟุตบอลแล้วมักจะเป็นทีมตรงข้ามที่ชนะ ชอบเล่นมุกควาย มุกแป๊ก แต่ก็มีมุมที่คอยให้กำลังใจผู้คนทั่วไปที่เข้ามาอ่าน SNS ของเธอ")
+    let audioNatherine = document.getElementById("natherinemp3")
+    return audioNatherine.paused ? audioNatherine.play() : audioNatherine.pause()
 }
 
-// let natherinestop = () => {
-//     let audioNatherine = document.createElement("audio")
-//     audioNatherine.setAttribute("src", "natherine.mp3")
-//     audioNatherine.stop()
-// }
-
 let wee = () => {
-    // let audioWee = document.createElement("audio")
-    // audioWee.setAttribute("src","wee.mp3")
-    // audioWee.play()
-    alert("สาวน้อยลูกครึ่ง ดีกรีนักฟาดกระทะ ผู้ที่ติดเกม Player's Unknown BattleGround หรือ PubG เป็นที่สุด เธอมีคติประจำใจว่า 'คนจริงต้องลงโพชิงกิ' ")
+    let audioWee = document.getElementById("weemp3")
+    return audioWee.paused ? audioWee.play() : audioWee.pause()
 }
 
 let tarwaan = () => {
-    // let audioTarwaan = document.createElement("audio")
-    // audioTarwaan.setAttribute("src", "tarwaan.mp3")
-    // status = false
-    // if(status === false) {
-    //     status = true
-    //     console.log(status)
-    //     await audioTarwaan.play()
-    // }
-    alert("เด็กสาวแก่นโก๊ะ ผู้ที่มีลีลาการเต้นสุดเร้าใจ มีลีลาการร้องเพลงที่เริดไม่แพ้ใครไหนๆ เป็นที่จดจำจากการเต้นเพลงเชฟบ๊ะอย่างไม่ห่วงสวย จนได้ฉายา 'ตาหวานคนดีด' ")
+    let audioTarwaan = document.getElementById("tarwaanmp3")
+    return audioTarwaan.paused ? audioTarwaan.play() : audioTarwaan.pause()
 }
