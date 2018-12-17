4eaee197995356f60b276ad147eb010298c0ca07
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-12-17 19:34:17 +0700

    :tada: Initial Commit.

---
+++ main.js
@@ -0,0 +1,32 @@
+let natherine = () => {
+    // let tagname = document.getElementsByTagName("p")
+    // let audioNatherine = document.createElement("audio")
+    // audioNatherine.setAttribute("src","natherine.mp3")
+    // audioNatherine.setAttribute("preload","auto")
+    alert("Natherine BNK48 เป็นสาวเก่งที่มีชื่อเสียงจากการที่เธอทายผลฟุตบอลแล้วมักจะเป็นทีมตรงข้ามที่ชนะ ชอบเล่นมุกควาย มุกแป๊ก แต่ก็มีมุมที่คอยให้กำลังใจผู้คนทั่วไปที่เข้ามาอ่าน SNS ของเธอ")
+}
+
+// let natherinestop = () => {
+//     let audioNatherine = document.createElement("audio")
+//     audioNatherine.setAttribute("src", "natherine.mp3")
+//     audioNatherine.stop()
+// }
+
+let wee = () => {
+    // let audioWee = document.createElement("audio")
+    // audioWee.setAttribute("src","wee.mp3")
+    // audioWee.play()
+    alert("สาวน้อยลูกครึ่ง ดีกรีนักฟาดกระทะ ผู้ที่ติดเกม Player's Unknown BattleGround หรือ PubG เป็นที่สุด เธอมีคติประจำใจว่า 'คนจริงต้องลงโพชิงกิ' ")
+}
+
+let tarwaan = async () => {
+    // let audioTarwaan = document.createElement("audio")
+    // audioTarwaan.setAttribute("src", "tarwaan.mp3")
+    // status = false
+    // if(status === false) {
+    //     status = true
+    //     console.log(status)
+    //     await audioTarwaan.play()
+    // }
+    alert("เด็กสาวแก่นโก๊ะ ผู้ที่มีลีลาการเต้นสุดเร้าใจ มีลีลาการร้องเพลงที่เริดไม่แพ้ใครไหนๆ เป็นที่จดจำจากการเต้นเพลงเชฟบ๊ะอย่างไม่ห่วงสวย จนได้ฉายา 'ตาหวานคนดีด' ")
+}
